c0a5b8aed979c7e7ac6156add7bc9c53d79ecaf6
Tony Carnese <Tony Carnese>
2017-02-21 21:53:31 -0500

    prompts actually interact with db!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,29 +4,80 @@ var inquirer = require('inquirer');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'C0c0nuts!',
     database: 'bamazon_db',
 });
 
 connection.connect();
+console.log("");
+console.log("*********************")
+console.log("Welcome to Bamazon!")
+console.log("*********************")
+console.log("");
 
-connection.query('SELECT * from products', function(error, results, fields) {
+function checkIdExists(productsList, id) {
+     for (var i = 0; i < productsList.length; i++) {
+            // console.log(productsList[i], data.id)
+            if (productsList[i].id === parseInt(id)) {
+                askHowMany(productsList[i]);
+            } 
+            // else {
+            //     console.log(results[i])
+            // }
+        }
+}
+
+function askHowMany(product) {
     inquirer.prompt([{
-        type: "input",
-        name: "product",
-        message: "Put the id of the product that you want to purchase!"
-    }]).then(function(data) {
-        var product = data.product;
-
-        connection.query('SELECT * from products', function(error, results, fields) {
-            inquirer.prompt([{
                 type: "input",
                 name: "quantity",
                 message: "Set the quantity you wish to purchase:"
             }]).then(function(data) {
-		  console.log("it worked!");
-                })
-                    connection.end();
-                });
-            });
-        });
+                if (product.stock_quantity >= data.quantity) {
+                    updateQuantity(data.quantity, product);
+                }
+                else {
+                    console.log("Not enough, try again");
+                    askHowMany(product);
+                }
+                
+            })
+                    // connection.end();
+}
+
+function updateQuantity(quantity, product) {
+    connection.query('UPDATE products SET stock_quantity=' + (product.stock_quantity - quantity) + ' WHERE id=' + product.id, function(error, results) {
+        console.log(error);
+        console.log(results);
+    });
+}
+
+connection.query('SELECT * from products', function(error, results, fields) {
+
+console.log(results);
+
+    inquirer.prompt([{
+        type: "input",
+        name: "id",
+        message: "Put the id of the product that you want to purchase!"
+    }]).then(function(data) {
+
+        checkIdExists(results, data.id);
+       
+
+        // connection.query('SELECT * from products', function(error, results, fields) {
+        //     inquirer.prompt([{
+        //         type: "input",
+        //         name: "quantity",
+        //         message: "Set the quantity you wish to purchase:"
+        //     }]).then(function(data) {
+        //         // console.log(results.product_id)
+
+        //         })
+        //             connection.end();
+ 
+ 
+
+        //         });//closing line 29 connection query
+            });//closing product_id function
+        });//closing line 18 connection query
